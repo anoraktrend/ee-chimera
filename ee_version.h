4f970ca7e15aaa929ebcebba73d315d2c459ec22
Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>
2026-04-15 03:35:08 -0700

    chore: bump version to 1.5.8
    Signed-off-by: Lucy Ada Randall <19198652+anoraktrend@users.noreply.github.com>

--- ee_version.h
+++ ee_version.h
@@ -5,7 +5,7 @@
  |	provide a version number for ee
  */
 
-#define EE_VERSION "1.5.3"
-#define DATE_STRING "$Date: 2026/04/14 18:45:00 $"
+#define EE_VERSION "1.5.8"
+#define DATE_STRING "$Date: 2026/04/14 19:30:00 $"
 
 #endif
